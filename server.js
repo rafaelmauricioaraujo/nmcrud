71d77f34b180bc06a7061cd3024e8f3963818022
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2019-07-28 18:20:41 -0300

    adicionando body-parser para lidar com os parametros

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded(
+    {extended: true}
+    )
+);
 
 app.get('/show', function(req, res){
-    res.render('index.ejs');
+    console.log(req.body)
 });
 
 app.listen(3000, function(){
