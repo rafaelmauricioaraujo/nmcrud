863f5947e1e06b49f5c68bb58f08daed4a96d1c3
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2019-07-28 18:11:55 -0300

    criando um index e renderizando com o ejs

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/show', function(req, res){
+    res.render('index.ejs');
+});
+
 app.listen(3000, function(){
     console.log('Server running on port 3000');
-})
+});
