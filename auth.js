50e37ab247fc6361b3ba9ee6a1411dbdca35ea5f
Rafael <rafael.mauricio.araujo@gmail.com>
2019-08-07 15:01:37 -0300

    adicionando funcoes de busca por username e por id

--- auth.js
+++ auth.js
@@ -2,5 +2,18 @@ const bcrypt = require('bcrypt')
 const LocalStrategy = require('passport-local').Strategy
 
 module.exports = function(passport){
+    function findUser(username, callback){
+        global.db.collection('users').findOne({'username':username}, function(err){
+            callback(err, doc);
+        });
+    }
+
+    function findUserById(id, callback){
+        const ObjectId = require('mongodb').ObjectID;
+        global.db.collection('users').findOne({_id:ObjectId(id)}, (err, doc)=>{
+            callback(err, doc);
+        });
+    }
+
     
 }
